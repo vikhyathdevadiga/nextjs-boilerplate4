6772bc7ab96516eb106c526f7700f40aa988052b
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-04-09 11:56:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+      return [
+        {
+source: "/studio/api/:path*",
+destination: "/api/:path*",
+},
+{
+source: "/:path*",
+has: [
+{
+type: "host",
+value: "nextjs-boilerplate.*",
+},
+],
+destination: "https://www.google.com",
+},
+      ];
+    }
 };
 
 export default nextConfig;
