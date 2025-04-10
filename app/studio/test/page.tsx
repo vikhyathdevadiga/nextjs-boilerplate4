625208c2770fa989efae937d4f6bc8cd110a38b3
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-04-10 09:46:26 +0530

    Update page.tsx

--- app/studio/test/page.tsx
+++ app/studio/test/page.tsx
@@ -14,6 +14,9 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
+
+            ------------ REWRITE PAGE ------------
+            
             Get started by editing STUDIO {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
