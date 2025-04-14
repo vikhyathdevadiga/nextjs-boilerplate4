a17c784dc4a9d8ee4a4addb61c200e41fa6cf6e7
Vikhyath S <49331452+vikhyathdevadiga@users.noreply.github.com>
2025-04-14 22:37:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,39 +3,38 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-      return [
+    return {
+      beforeFiles: [
         {
-source: "/studio/api/:path*",
-destination: "/api/:path*",
-},
-  
-
-              beforeFiles: [
+          source: "/",
+          has: [
+            {
+              type: "host",
+              value: "nextjs-boilerplate.*",
+            },
+          ],
+          destination: "/studio/test",
+        },
         {
-        source: "/",
-        has: [
-          {
-            type: "host",
-            value: "nextjs-boilerplate.*",
-          },
-        ],
-        destination: "/studio/test",
-      }
-        
-        ],
-
-{
-source: "/:path*",
-has: [
-{
-type: "host",
-value: "nextjs-boilerplate.*",
-},
-],
-destination: "/studio/:path*",
-},
-      ];
-    }
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "nextjs-boilerplate.*",
+            },
+          ],
+          destination: "/studio/:path*",
+        }
+      ],
+      afterFiles: [
+        {
+          source: "/studio/api/:path*",
+          destination: "/api/:path*",
+        }
+      ],
+      fallback: []
+    };
+  }
 };
 
 export default nextConfig;
